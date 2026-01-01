0df3b40538998dd36e6db579951e148ef980f8f9
olusegun <olusegunbamgbelu755@gmail.com>
2026-01-01 18:30:08 +0100

    feat:pie chart done

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,8 +1,10 @@
+import CategoryStats from "./categoty-stats";
 import SectionCards from "./section-card";
+import ChartPieDonutText from "./shadcn-piechart";
 
 export default function Dashboard() {
   return (
-    <div className="flex-1 p-5 flex flex-col gap-3 md:p-8 md:gap-5 overflow-y-auto h-screen">
+    <div className="flex-1 p-5 flex flex-col gap-3 md:p-8 md:gap-5 max-sm:overflow-y-auto max-sm:h-screen">
       <div className="flex">
         <div className="flex flex-col gap-1">
           <h1 className="text-2xl font-bold md:text-3xl">Dashboard</h1>
@@ -11,7 +13,14 @@ export default function Dashboard() {
           </p>
         </div>
       </div>
-      <SectionCards/>
+      <SectionCards />
+      <div className="w-full flex">
+        <div className="flex flex-col justify-center items-center w-full md:w-75 bg-card rounded-2xl p-4">
+          <h3 className="text-xl font-bold">Stock by Category</h3>
+          <ChartPieDonutText />
+          <CategoryStats/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/dashboard/shadcn-piechart.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart"
+
+export const description = "A donut chart with text"
+
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 190, fill: "var(--color-other)" },
+]
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "var(--chart-1)",
+  },
+  safari: {
+    label: "Safari",
+    color: "var(--chart-2)",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "var(--chart-3)",
+  },
+  edge: {
+    label: "Edge",
+    color: "var(--chart-4)",
+  },
+  other: {
+    label: "Other",
+    color: "var(--chart-5)",
+  },
+} satisfies ChartConfig
+
+export default function ChartPieDonutText() {
+  const totalVisitors = chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+ 
+  return (
+    <Card className="flex flex-col max-w-70 w-full h-60 border-none rounded-none shadow-none py-0">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-62.5"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={75}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Visitors
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
